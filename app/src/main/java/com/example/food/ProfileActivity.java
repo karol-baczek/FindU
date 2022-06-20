88e6705a6d6bb001b19f4edad00a135da9bb857f
karol-baczek <baczekkarol@gmail.com>
2022-06-20 16:36:08 +0200

    Q

--- app/src/main/java/com/example/food/ProfileActivity.java
+++ app/src/main/java/com/example/food/ProfileActivity.java
@@ -67,13 +67,13 @@ public class ProfileActivity extends AppCompatActivity implements ActivityCompat
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED) {
-            // Permission is already available, show restaurants
+
             Snackbar.make(mLayout,
-                    "Location permission available. Show restaurants.",
+                    "Location permission available.",
                     Snackbar.LENGTH_SHORT).show();
             start();
         } else {
-            // Permission is missing and must be requested.
+
             requestLocationPermission();
         }
     }
@@ -83,15 +83,15 @@ public class ProfileActivity extends AppCompatActivity implements ActivityCompat
                                            @NonNull  int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == PERMISSION_REQUEST_LOCATION) {
-            // Request for location permission.
+
             if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // Permission has been granted. Start preview Activity.
+
                 Snackbar.make(mLayout, "Location permission granted.",
                         Snackbar.LENGTH_SHORT)
                         .show();
                 start();
             } else {
-                // Permission request was denied.
+
                 Snackbar.make(mLayout, "Location permission request was denied.",
                         Snackbar.LENGTH_SHORT)
                         .show();
@@ -100,12 +100,10 @@ public class ProfileActivity extends AppCompatActivity implements ActivityCompat
     }
 
     private void requestLocationPermission() {
-        // Permission has not been granted and must be requested.
+
         if (ActivityCompat.shouldShowRequestPermissionRationale(this,
                 Manifest.permission.ACCESS_FINE_LOCATION)) {
-            // Provide an additional rationale to the user if the permission was not granted
-            // and the user would benefit from additional context for the use of the permission.
-            // Display a SnackBar with a button to request the missing permission.
+
             Snackbar.make(mLayout, "Location access is required.",
                     Snackbar.LENGTH_INDEFINITE).setAction("OK", new View.OnClickListener() {
                 @Override
@@ -121,7 +119,7 @@ public class ProfileActivity extends AppCompatActivity implements ActivityCompat
             Snackbar.make(mLayout,
                     "Permission is not available. Requesting location permission.",
                     Snackbar.LENGTH_SHORT).show();
-            // Request the permission. The result will be received in onRequestPermissionResult().
+
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
                     PERMISSION_REQUEST_LOCATION);
         }
